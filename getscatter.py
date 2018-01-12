6efa61956db0db3b7d883e7aec28e09a7298f7f5
wagenaa <wagenaa@pc160111.knmi.nl>
2018-01-12 12:44:14 +0000

    m

--- getscatter.py
+++ getscatter.py
@@ -1,22 +1,34 @@
+
+#load modules
 import os
 from scipy.stats import gaussian_kde
 import glob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
+
+
+
 input_files = {}
+
+#define years and loop over the array
+
 years = ("2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016")
+
 for i in years:
     
     input_files['main_folder1'] = "/usr/people/wagenaa/scatterEPS/" + str(i) + "/06514/" 
     tf_Folder_list = glob.glob(input_files['main_folder1'] + "/*/")
     
+#function for normalized distance as a function of ensemble mean surge level panel plots
 
 def panel_plot_Hobs(year, folder_list):
-
+    #main fig
     fig = plt.figure()
     index = 1
+    #adjust size axis titles
     plt.rcParams["axes.titlesize"] = 10
+    
     for folder in folder_list:
         print folder    
         basename1 = os.path.basename(os.path.normpath(str(folder)))
@@ -26,31 +38,37 @@ def panel_plot_Hobs(year, folder_list):
         for file in filelist:
             print file    
 
-
+            #make dataframe
             df = pd.read_csv(file, sep = "\t")
             print df
+            #exclude cases where sigma != 0 
 
             df = df[df['sigma'] != 0]
+ 
+            #normalized distance calc
             y = (df['hmean'] - df['hobs'])/df['sigma']
         
             print y
             x = df['hmean']
             print x
-    # Calculate the point density
+            # Calculate the point density based on Kernel Density Estimation
             xy = np.vstack([x,y])
             
             z = gaussian_kde(xy)(xy)
 
-    # Sort the points by density, so that the densest points are plotted last
+            # Sort the points by density, so that the densest points are plotted last
             idx = z.argsort()
             x, y, z = x[idx], y[idx], z[idx]
+            
+            #add subplot in figure (4X5 total)
             ax = fig.add_subplot(4, 5, index)
-        
+            #make scatterplot
             cax = ax.scatter(x, y, c=z, s=10, edgecolor = 'None', cmap = 'plasma')
-        #fig.text(0.5, 0.04, 'Hobs [cm]', ha='center', fontsize = 12)
-        #fig.text(0.04, 0.5, '(Hmean - Hobs) / sigma [-]', va='center', rotation='vertical', fontsize = 12)
-            ax.set_title(basename1 + "hrs", size = 10, weight = 'bold')
+        
+            ax.set_title(basename1 + "h", size = 10, weight = 'bold')
             index = index +1 
+
+            #subplot parameters
             plt.subplots_adjust(hspace = .4, wspace = 0.4)
             plt.locator_params('y', nbins=4)
             plt.locator_params('x', nbins=3)
@@ -69,29 +87,35 @@ def panel_plot_Hobs(year, folder_list):
             #plt.show()
             #plt.pcolormesh(xi, yi, zi.reshape(xi.shape), cmap=plt.cm.Greens_r)
 
+    #Make colorbar on the right side of the figure
 
     cbar_ax = fig.add_axes([0.925, 0.25, 0.015, 0.5])
     cbar = fig.colorbar(cax, cax=cbar_ax) 
     cbar.ax.tick_params(labelsize=8)
+    
+    #main axis labels
     fig.text(0.5, 0.04, 'Hmean [cm]', ha='center', fontsize = 12)
     fig.text(0.04, 0.5, '(Hmean - Hobs) / sigma [-]', va='center', rotation='vertical', fontsize = 12)
+    #main fig title
     fig.suptitle(str(year), fontsize=18, weight = 'bold')
 
     plt.savefig("/usr/people/wagenaa/ScatterDensityFigures/scatter_density_06514_" + str(year) + ".pdf")
-#plt.show()
 
+
+
+#loop over years array
 for i in years:
     
     input_files['main_folder1'] = "/usr/people/wagenaa/scatterEPS/" + str(i) + "/06514/" 
     tf_Folder_list = glob.glob(input_files['main_folder1'] + "/*/")
+    
+    #use the function to make panel plots
     panel_plot_Hobs(i, tf_Folder_list)
 
 
+#function to generate panel plots with  normalized distance as a function of forecast year 
 
 def panel_plot_Years():
-   # df_tot = pd.DataFrame()
-    #years = ("0-12", "", "2011", "2012", "2013", "2014", "2015", "2016")
-    #for i in years:
     input_files['main_folder1'] = "/usr/people/wagenaa/scatterEPS_years_LT" + "/06514/" 
     fig = plt.figure()
     index = 1
@@ -100,7 +124,10 @@ def panel_plot_Years():
     plt.rcParams["axes.titlesize"] = 10    
     for folder in folder_list:
         df_tot = pd.DataFrame()
+        
+        #add subplot in figure (4x5 total)
         ax = fig.add_subplot(4, 5, index)
+        
         print folder    
         basename1 = os.path.basename(os.path.normpath(str(folder)))
         print basename1
@@ -112,55 +139,42 @@ def panel_plot_Years():
             filelist = glob.glob(year_folder +  "/*.txt")
             for file in filelist:
                 df = pd.read_csv(file, sep = "\t")
-                #print df
-
                 df = df[df['sigma'] != 0]
                 df['bias'] = (df['hmean'] - df['hobs'])/df['sigma']
-                #y = (df['hmean'] - df['hobs'])/df['sigma']
                 df['year'] = float(basename_year)
-                #print y
-                #x = df['hmean']
-                #print x
                 df_tot = df_tot.append(df)
-                #print df_tot
-                #print df_tot
                 y = (df['hmean'] - df['hobs'])/df['sigma']
                 x = df['year']
-            # Calculate the point density
                 
-        #xy = np.vstack([x,y])
-        #print xy
+                # Calculate the point density
                 z = gaussian_kde(y)(y) 
                 print z
-            # Sort the points by density, so that the densest points are plotted last
+                
+                #Sort the points by density, so that the densest points are plotted last
                 idx = z.argsort()
                 x, y, z = x[idx], y[idx], z[idx]
                 ax = fig.add_subplot(4, 5, index)
 
                 cax = ax.scatter(x, y, c=z, s=10, edgecolor='', cmap = 'plasma')
-                #set(gca, 'XTick', [2009 2010 2011 2012 2013 2014 2015 2016])
-                #ax.set_xticklabels(['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'])
-                ax.set_title(basename1 + "hrs", size = 10, weight = 'bold')
-                #plt.colorbar(cax, ax = ax)        
+                
+                ax.set_title(basename1 + "h", size = 10, weight = 'bold')
+            
         index = index + 1
         plt.subplots_adjust(hspace = .4, wspace = 0.4)
         plt.locator_params('y', nbins=4)
         plt.locator_params('x', nbins=3)
-        plt.ylim((-3.0,3.0))
-        #plt.xlim((2009,2016))
-        #plt.xticks(range(8), [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016])
-        
+        plt.ylim((-3.0,3.0))        
         plt.tick_params(axis = 'y', labelsize = 9)
         plt.tick_params(axis = 'x', labelsize = 7)
-        #plt.colorbar()
     cbar_ax = fig.add_axes([0.925, 0.25, 0.015, 0.5])
     cbar = fig.colorbar(cax, cax=cbar_ax) 
     cbar.ax.tick_params(labelsize=8)
     fig.text(0.5, 0.04, 'Time [years]', ha='center', fontsize = 12)
     fig.text(0.04, 0.5, '(Hmean - Hobs) / sigma [-]', va='center', rotation='vertical', fontsize = 12)
-    #fig.suptitle(str(year), fontsize=18, weight = 'bold')
-
     plt.savefig("/usr/people/wagenaa/ScatterDensityFigures/scatter_density_06514_years_LT_test.pdf")
+
+
+#call the function
 panel_plot_Years()
 
 
--- getscatter.sh
+++ getscatter.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/ksh
+
+#define stations, lead-time and years arrays
+
 set -A stations 06511 06512 06514 06515 06520
 #set -A tfs 0:48 48:84 84:120 120:168 168:240 
 set -A tfs 0:12 12:24 24:36 36:48 48:60 60:72 72:84 84:96 96:108 108:120 120:132 132:144 144:156 156:168 168:180 180:192 192:204 204:216 216:228 228:240
@@ -6,10 +9,13 @@ set -A tfs 0:12 12:24 24:36 36:48 48:60 60:72 72:84 84:96 96:108 108:120 120:132
 #set -A years 2008 2009 2010 2011 2012 2013 2014 2015 2016
 set -A years 2009 2010 2011 2012 2013 2014 2015 2016 
 
+#function for MYSQL query
+
 query ()
 {
 (
 
+# loop over stations array
 
 for station in "${stations[@]}"; do
   
@@ -18,6 +24,8 @@ for station in "${stations[@]}"; do
   echo $1
   echo $2
   echo $3
+  #make directories
+
   if [ ! -d /usr/people/wagenaa/scatterEPS_HT/$3/$station ]; then
     mkdir -p /usr/people/wagenaa/scatterEPS_HT/$3/$station 
   fi
@@ -33,6 +41,8 @@ for station in "${stations[@]}"; do
   #if [ ! -d /usr/people/wagenaa/CRPS_members/13-16/$station/$1-$2 ]; then
     #mkdir -p /usr/people/wagenaa/CRPS_members/13-16/$station/$1-$2
   #fi
+  
+  #do MYSQL query
   echo "select dtg, tastr, hastr, hmean, sigma, hobs from eps"$station"_"$3" where HOUR(timediff(tastr, dtg))>$1 AND HOUR(timediff(tastr, dtg))<$2 AND hobs IS NOT NULL" |mysql --defaults-file=/usr/people/wagenaa/.mysql/ontwrd.cnf> /usr/people/wagenaa/scatterEPS/$3/$station/$1-$2/EPS_"$station"_$1-$2_.txt 
 done
 )\
@@ -79,6 +89,7 @@ done
 }
 
 
+#loop over years and lead-time and use it  as input for query function
 for year in "${years[@]}"; do 
   for tf in ${tfs[*]};do
     echo $tf
